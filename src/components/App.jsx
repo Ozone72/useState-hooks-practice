3a6d1d0e94ed26d4b5ba5aac348a6487c412ac71
Orin Fletcher <orin.fletcher@gmail.com>
2021-01-14 13:42:19 -0800

    finishes challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // calls updateTime ever second.
+  setInterval(updateTime, 1000);
+
+  // sets initial value for useState hook
+  const now = new Date().toLocaleTimeString("en-US", { hour12: false });
+
+  // destructured array, where time is the value, and setTime is what acts on initial time value.  Initial state is set to current time
+  const [time, setTime] = useState(now);
+
+  // the updateTime function is tied to the onClick button event below
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString("en-US", { hour12: false });
+    // setTime updates the value of time each time updateTime is called.
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      {/* time is updated in the h1*/}
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
